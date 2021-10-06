9d44dccd36bfad4341cc9dbcc398a81f06c60f4d
Mayur Mohurle <mayur.mohurle880606@gmail.com>
2021-10-06 19:36:57 +0530

    Deployment - 2

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { SET_ALERT, REMOVE_ALERT } from './types';
 import { v4 as uuid } from 'uuid';
 
 export const setAlert =
-  (msg, alertType, timeout = 5000) =>
+  (msg, alertType, timeout = 1000) =>
   (dispatch) => {
     const id = uuid();
     dispatch({
